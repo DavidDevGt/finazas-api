a4d657516ad21d5f3f0b7499f3d6de79ea26adeb
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-22 23:18:42 -0600

    Refactor BaseModel constructor and fix variable names

--- models/BaseModel.js
+++ models/BaseModel.js
@@ -1,42 +1,59 @@
+const Database = require("../config/Database");
+
 class BaseModel {
-    constructor(db, tableName) {
-        this.db = db;
-        this.tableName = tableName;
-    }
+  constructor(tableName) {
+    this.db = new Database();
+    this.tableName = tableName;
+  }
 
-    findAll() {
-        return this.db.query(`SELECT * FROM ${this.tableName}`);
-    }
+  findAll() {
+    return this.db.query(`SELECT * FROM ${this.tableName}`);
+  }
 
-    findById(id) {
-        return this.db.query(`SELECT * FROM ${this.tableName} WHERE id = ?`, [id]);
-    }
+  findById(id) {
+    return this.db.query(`SELECT * FROM ${this.tableName} WHERE id = ?`, [id]);
+  }
 
-    findOne(conditions = {}, fields = '*') {
-        const whereClause = Object.keys(conditions).map(key => `${key} = ?`).join(' AND ');
-        const values = Object.values(conditions);
-        return this.db.query(`SELECT ${fields} FROM ${this.tableName} WHERE ${whereClause}`, values);
-    }
+  findOne(conditions = {}, fields = "*") {
+    const whereClause = Object.keys(conditions)
+      .map((key) => `${key} = ?`)
+      .join(" AND ");
+    const values = Object.values(conditions);
+    return this.db.query(
+      `SELECT ${fields} FROM ${this.tableName} WHERE ${whereClause}`,
+      values
+    );
+  }
 
-    create(data) {
-        const keys = Object.keys(data);
-        const values = Object.values(conditions);
-        const placeholders = keys.map(key => '?').join(', ');
-        return this.db.query(`INSERT INTO ${this.tableName} (${keys.join(', ')}) VALUES (${placeholders})`, values);
-    }
+  create(data) {
+    const keys = Object.keys(data);
+    const values = Object.values(conditions);
+    const placeholders = keys.map((key) => "?").join(", ");
+    return this.db.query(
+      `INSERT INTO ${this.tableName} (${keys.join(
+        ", "
+      )}) VALUES (${placeholders})`,
+      values
+    );
+  }
 
-    update(id, data) {
-        const updates = Object.keys(data).map(key => `${key} = ?`).join(', ');
-        const values = [...Object.values(data), id];
-        return this.db.query(`UPDATE ${this.tableName} SET ${updates} WHERE id = ?`, values);
-    }
+  update(id, data) {
+    const updates = Object.keys(data)
+      .map((key) => `${key} = ?`)
+      .join(", ");
+    const values = [...Object.values(data), id];
+    return this.db.query(
+      `UPDATE ${this.tableName} SET ${updates} WHERE id = ?`,
+      values
+    );
+  }
 
-    delete(id) {
-        return this.db.query(`DELETE FROM ${this.tableName} WHERE id = ?`, [id]);
-    }
+  delete(id) {
+    return this.db.query(`DELETE FROM ${this.tableName} WHERE id = ?`, [id]);
+  }
 
-    // Método para realizar búsquedas complejas, si es necesario
-    dbQuery(sql, params) {
-        return this.db.query(sql, params);
-    }
+  // Método para realizar búsquedas complejas, si es necesario
+  dbQuery(sql, params) {
+    return this.db.query(sql, params);
+  }
 }
