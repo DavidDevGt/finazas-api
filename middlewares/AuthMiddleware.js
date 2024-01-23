7eb5574bc73f0971efc2f4a418ed57e8b7dff2e6
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-23 00:03:41 -0600

    Add AuthMiddleware and EncryptService

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -1,3 +1,18 @@
-class AuthMiddleware {
-    
-}
+const jwt = require('jsonwebtoken');
+
+const SECRET_KEY = process.env.JWT_SECRET;
+
+const authMiddleware = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.userData = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Autenticación fallida'
+        });
+    }
+};
+
+module.exports = authMiddleware;
